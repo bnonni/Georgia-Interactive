3ea8b0c237ac2f73c742cc66db593ca561b09a9c
bnonni <github@nonni.dev>
2025-05-05 12:26:21 -0400

    fixing broken links

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const url = require('url');
 const hostname = '127.0.0.1';
 const fs = require('fs');
-const port = 8888;
+const port = 8000;
 var reload = require('reload');
 
 const server = http.createServer((req, res) => {
